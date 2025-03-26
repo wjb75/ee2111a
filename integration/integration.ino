0337417bfad800bdb2550f80d7b9ba7cafbe5d90
wjb75 <178544132+wjb75@users.noreply.github.com>
2025-03-26 12:23:50 +0800

    Update integration.ino

--- integration/integration.ino
+++ integration/integration.ino
@@ -12,13 +12,85 @@ MeDCMotor rightMotor(M2);
 unsigned long lastTurnTime = 0;  
 unsigned long AVOID_TIME = TURN_DELAY * 1.5;
 
+float getFilteredDistance(int numSamples = 3);
+void moveForward(int speed);
+void turnLeft(int speed, int delayTime);
+void stopMotors();
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    float distance = getFilteredDistance();  
+    Serial.print("Distance: ");
+    Serial.println(distance);
 
+    
+    if (millis() - lastTurnTime < AVOID_TIME) {
+        Serial.println("Recently turned, skipping obstacle check...");
+        moveForward(100);
+        return;
+    }
+
+    if (distance > 0 && distance <= OBSTACLE_DISTANCE) {  
+        stopMotors();
+        delay(300);
+        
+        
+        float newDistance = getFilteredDistance();
+        if (newDistance > 0 && newDistance <= OBSTACLE_DISTANCE) {
+            Serial.println("Confirmed obstacle! Turning...");
+            turnLeft(TURN_SPEED, TURN_DELAY); 
+        } else {
+            Serial.println("False alarm, continue moving...");
+        }
+    } else {
+        moveForward(100); 
+    }
+}
+
+
+float getFilteredDistance(int numSamples = 3) {
+    float totalDistance = 0;
+    int validSamples = 0;
+
+    for (int i = 0; i < numSamples; i++) {
+        float d = ultrasonic.distanceCm();
+        if (d > 0) {  
+            totalDistance += d;
+            validSamples++;
+        }
+        delay(50);  
+    }
+
+    return (validSamples > 0) ? totalDistance / validSamples : 0;  
+}
+
+
+void moveForward(int speed) {
+    leftMotor.run(speed);
+    rightMotor.run(speed);
+}
+
+// 左转 170°
+void turnLeft(int speed, int delayTime) {
+    Serial.println("Turning left 170°...");
+    leftMotor.run(-speed);
+    rightMotor.run(speed);
+    delay(delayTime);
+    stopMotors();
+    delay(500);  
+    lastTurnTime = millis();  
+}
+
+
+void stopMotors() {
+    leftMotor.run(0);
+    rightMotor.run(0);
 }
