33071fa283d7aabc93b73b0667e9dffffddabf36
wjb75 <178544132+wjb75@users.noreply.github.com>
2025-04-01 11:33:52 +0800

    Update integration.ino

--- integration/integration.ino
+++ integration/integration.ino
@@ -3,12 +3,16 @@
 MeUltrasonicSensor ultrasonic(PORT_2);  
 MeLineFollower lineFinder(PORT_3); /* Line Finder module can only be connected to PORT_3, PORT_4, PORT_5, PORT_6 of base shield. */
 MeDCMotor leftMotor(M1);                
-MeDCMotor rightMotor(M2);               
+MeDCMotor rightMotor(M2);    
+MeBuzzer buzzer;
+
 
 #define TURN_SPEED 100      
 #define TURN_DELAY 1500     
 #define OBSTACLE_DISTANCE 8 
 #define ALIGN_DELAY 100
+#define NOTE_DS8 4978
+#define NOTE_A3  220
 #define OPAMP_PIN A1
 #define FORCE_SENSOR A0
 
@@ -33,6 +37,8 @@ void loop() {
   // first check if the dust bag is full by reading the force sensors.
   force = analogRead(FORCE_SENSOR);
   //IF selection statement : do sth if force reading exceeds certain val
+  buzzer.tone(NOTE_DS8,20);
+
 
   ir = analogRead(OPAMP_PIN); //check if the ir emitter is in certain distance ahead
   // do sth if the ir station is in front
@@ -43,7 +49,7 @@ void loop() {
 
    int sensorState = lineFinder.readSensors();
    if(sensorState ==  S1_OUT_S2_OUT){
-      Serial.println("Sensor left(1) and right(2) are outside of black line. Safe to go!");
+      //Serial.println("Sensor left(1) and right(2) are outside of black line. Safe to go!");
    }
 
    while (sensorState == S1_IN_S2_OUT || sensorState == S1_OUT_S2_IN  ) {
@@ -55,11 +61,11 @@ void loop() {
     //  Serial.println("Sensor left(1) and right(2)  are on black line"); 
     //  break;
     case S1_OUT_S2_IN: 
-      Serial.println("Sensor right(2) is on black line");  //needs to turn right to align first
+      //Serial.println("Sensor right(2) is on black line");  //needs to turn right to align first
       turnLeft(-TURN_SPEED, ALIGN_DELAY);
       break;
     case S1_IN_S2_OUT: 
-      Serial.println("Sensor left(1) is on black line"); //needs to turn left to align first
+      //Serial.println("Sensor left(1) is on black line"); //needs to turn left to align first
       turnLeft(TURN_SPEED, ALIGN_DELAY);
       break;
     //case S1_OUT_S2_OUT: 
@@ -72,13 +78,14 @@ void loop() {
 
   if (sensorState == S1_IN_S2_IN)
   {
+    delay(100);
     turnLeft(TURN_SPEED, TURN_DELAY); 
 
   }
 
     float distance = getFilteredDistance();  
-    Serial.print("Distance: ");
-    Serial.println(distance);
+    //Serial.print("Distance: ");
+    //Serial.println(distance);
 
 
     if (distance > 0 && distance <= OBSTACLE_DISTANCE) {  
@@ -88,10 +95,10 @@ void loop() {
         
         float newDistance = getFilteredDistance();
         if (newDistance > 0 && newDistance <= OBSTACLE_DISTANCE) {
-            Serial.println("Confirmed obstacle! Turning...");
+            //Serial.println("Confirmed obstacle! Turning...");
             turnLeft(TURN_SPEED, TURN_DELAY); 
         } else {
-            Serial.println("False alarm, continue moving...");
+            //Serial.println("False alarm, continue moving...");
         }
     } else {
         moveForward(100); 
