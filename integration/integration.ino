a7a43e69a1a6165a45a314e15d5d93c49ae2171b
wjb75 <178544132+wjb75@users.noreply.github.com>
2025-04-07 10:33:25 +0800

    added digital pins
    added digitalpins 11 12 for the force and ir sensor

--- integration/integration.ino
+++ integration/integration.ino
@@ -13,13 +13,20 @@ MeBuzzer buzzer;
 #define ALIGN_DELAY 100
 #define NOTE_DS8 4978
 #define NOTE_A3  220
-#define OPAMP_PIN A2
-#define FORCE_SENSOR A3
+
+#define OPAMP_PIN A1
+#define FORCE_SENSOR A0
+#define FORCE_DIGITAL 11
+#define IR_DIGITAL 12
+
 
 unsigned long lastTurnTime = 0;  
 unsigned long AVOID_TIME = TURN_DELAY * 1.5;
 int force = 0; // the reading from the force sensor
 int ir = 0; //reading from the ir op amp sensor
+int digipin11force;
+int digipin12ir;
+
 
 float getFilteredDistance(int numSamples = 3);
 void moveForward(int speed);
@@ -30,25 +37,60 @@ void stopMotors();
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
+  pinMode(FORCE_DIGITAL, INPUT);
+  pinMode(IR_DIGITAL, INPUT);
 }
 
 void loop() {
+
+analogRead(A0);      // Dummy read to flush the ADC
+delayMicroseconds(5); 
+int a0 = analogRead(A0);
+delayMicroseconds(5);          // A short delay for the ADC to settle
+force = analogRead(FORCE_SENSOR);
+  int a1 = analogRead(A1);
+  int a2 = analogRead(A2);
+  int a3 = analogRead(A3);
+
+  Serial.print("A0 is ");
+  Serial.println(A0);
+    Serial.print("A1 is ");
+  Serial.println(A1);
+    Serial.print("A2 is ");
+  Serial.println(A2);
+    Serial.print("A3 is ");
+  Serial.println(A3);
+
+  digipin11force = digitalRead(11);
+  
+  digipin12ir = digitalRead(12);
+
+  Serial.print("11 is");
+  Serial.println(digipin11force);
+  Serial.print("12 is");
+  Serial.println(digipin12ir);
+
+
   // first check if the dust bag is full by reading the force sensors.
   force = analogRead(FORCE_SENSOR);
   Serial.println(force);
   //IF selection statement : do sth if force reading exceeds certain val
-  if(force > 100){
+
+  if(digipin11force == 1){
     buzzer.tone(NOTE_DS8,20);
+    stopMotors();
     return;
   }
+  //buzzer.tone(NOTE_DS8,20);
+
 
 
   ir = analogRead(OPAMP_PIN); //check if the ir emitter is in certain distance ahead
   // do sth if the ir station is in front ( the voltage value is smaller than 2.5V)
-  //if(ir < 512){ 
-  //  return;
-  //}
+  if(digipin12ir == 1){ 
+    stopMotors();
+    return;
+  }
 
 
    int sensorState = lineFinder.readSensors();
